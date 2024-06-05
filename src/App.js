337b443286be68dc283c8bc51050525a41ce9515
ramesh inumula <inumularamesh@gmail.com>
2024-06-05 22:58:22 +0530

    removed practice code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext } from 'react';
 import Hangman from './components/Hangman';
-import Bind from './practice/Bind';
-import Context from './practice/Context';
-import Refs from './practice/Refs';
 export const userContext = createContext(null)
 
 function App() {
@@ -10,10 +7,7 @@ function App() {
   return (
     <userContext.Provider value={{name,setname}}>
     <div className="App">
-    {/* <Hangman /> */}
-    <Refs />
-    <Context />
-    <Bind />
+    <Hangman />
     </div>
     </userContext.Provider>
   );
